d976c83c6e5530c2d4c12630934cfd509fd6725b
StefanSakovc <stefansakovic49@gmail.com>
2023-04-28 14:47:34 +0200

    bug fixes

--- cypress/e2e/OOP.cy.js/pageObjects/webdrive-uni/home-page_OP.js
+++ cypress/e2e/OOP.cy.js/pageObjects/webdrive-uni/home-page_OP.js
@@ -3,7 +3,7 @@ class HomePage {
 
     visitHomepage(){
 
-        cy.visit(Cypress.env("webdriver-url"))
+        cy.visit(Cypress.env("webdriverUrl"))
     }
     clickOn_ContuctUsButton(){
     cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true })
